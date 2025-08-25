07af8d01c80a2dbf44b8c826966fd52445b48d04
Prashant Upadhyay <prashantupadhyay557@gmail.com>
2025-08-25 16:22:06 +0530

    chore: add provider.tf with azurerm backend and pinned provider versions

--- Environment/dev/provider.tf
+++ Environment/dev/provider.tf
@@ -1,17 +1,9 @@
 terraform {
-  required_version = ">= 1.6.0"
+  required_version = ">= 1.3.0"
+  backend "azurerm" {}
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.116"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6"
-    }
+    azurerm = { source = "hashicorp/azurerm", version = "~> 3.117" }
+    random  = { source = "hashicorp/random",  version = "~> 3.7" }
   }
 }
-
-provider "azurerm" {
-  features {}
-}
+provider "azurerm" { features {} }
