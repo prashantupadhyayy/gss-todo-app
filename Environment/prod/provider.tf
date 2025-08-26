e07f399c9014d3c86db6b968a6b9a4c3864405b9
Prashant Upadhyay <prashantupadhyay557@gmail.com>
2025-08-26 16:48:42 +0530

    Update provider.tf

--- Environment/prod/provider.tf
+++ Environment/prod/provider.tf
@@ -1,9 +1,14 @@
 terraform {
   required_version = ">= 1.3.0"
+
   backend "azurerm" {}
+
   required_providers {
     azurerm = { source = "hashicorp/azurerm", version = "~> 3.117" }
     random  = { source = "hashicorp/random",  version = "~> 3.7" }
   }
 }
-provider "azurerm" { features {} }
+
+provider "azurerm" {
+  features {}
+}
